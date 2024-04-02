8dd75219a67e772ff3a16b6076b0c3b945c5f0ad
surbhi400 <77582783+surbhi400@users.noreply.github.com>
2024-04-02 23:45:31 +0530

    debugging

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,17 @@
 import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container";
+import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
 import Button from "./Button";
-import PostForm from "./post-form/PostForm";
+import PostForm from "./Post-Form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
-import Input from "./Input";
+import Input from "./Header/Input";
 export {
     Header,
     Footer,
