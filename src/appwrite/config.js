ee06f7b17f00b716442792243f32f558540cd613
surbhi400 <77582783+surbhi400@users.noreply.github.com>
2024-04-04 23:56:46 +0530

    fixed a lot of errors

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,98 +1,104 @@
-import conf from '../conf/conf';
-import {Client, ID, Databases, Storage, Query } from "appwrite";
+import conf from '../conf/conf.js';
+import { Client, ID, Databases, Storage, Query } from "appwrite";
 
 export class Service{
-    client= new Client();
+    client = new Client();
     databases;
     bucket;
+    
     constructor(){
         this.client
         .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteprojectId);
-
-        this.databases = new Databases(this.clent);
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
 
     async createPost({title, slug, content, featuredImage, status, userId}){
-       
         try {
-            return await this.databases.createDocument(conf.appwriteDatabaseId,
-            conf.appwriteCollectionId,
-            slug,
-            {
-                title,
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
                 slug,
-                content,
-                featuredImage,
-                status,
-                userId,
-            
-            }
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
             )
         } catch (error) {
-            console.log("Appwrite service :: createPost :: error", error);
+            console.log("Appwrite serive :: createPost :: error", error);
         }
     }
-    async updatePost(slug,{title, content, featuredImage, status}){
+
+    async updatePost(slug, {title, content, featuredImage, status}){
         try {
-    return await this.databases.updateDocument(
-        conf.appwriteDatabaseId,
-        conf.appwriteCollectionId,
-        slug,
-        {
-            title,
-            content,
-            featuredImage,
-            status,
-        }
-    )
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+
+                }
+            )
         } catch (error) {
-            console.log("Appwrite service :: updatePost :: error", error);
+            console.log("Appwrite serive :: updatePost :: error", error);
         }
     }
+
     async deletePost(slug){
         try {
             await this.databases.deleteDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                slug,
-                
+                slug
+            
             )
-            return true;
+            return true
         } catch (error) {
-            console.log("Appwrite service :: deletePost :: error", error);
-            return false;
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false
         }
     }
+
     async getPost(slug){
         try {
-            return await this.databases.deleteDocument(
+            return await this.databases.getDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                slug,
+                slug
+            
             )
         } catch (error) {
-            console.log("Appwrite service :: deletePost :: error", error);
-            return false;
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false
         }
     }
-    async getPosts(queries = [Query.equal("status","active")]){
+
+    async getPosts(queries = [Query.equal("status", "active")]){
         try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 queries,
+                
+
             )
         } catch (error) {
-            console.log("Appwrite service :: getPosts :: error", error);
-            return false;
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
         }
     }
 
     // file upload service
 
-    async UploadFile(file){
+    async uploadFile(file){
         try {
             return await this.bucket.createFile(
                 conf.appwriteBucketId,
@@ -100,30 +106,32 @@ export class Service{
                 file
             )
         } catch (error) {
-            console.log("Appwrite service :: uploadFile :: error", error);
-            return false;
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
         }
     }
+
     async deleteFile(fileId){
-       try{
-          await this.bucket.deleteFile(
-          conf.appwriteBucketId, 
-          fileId
-          )  
-        return true
-       }
-       catch(error){
-            console.log("Appwrite service :: uploadFile :: error", error);
-            return false;
-       }
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false
+        }
     }
-    async filePreview(fileId){
-       return this.bucket.getFilePreview(
-        conf.appwriteBucketId,
-        fileId
-       )
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
     }
 }
 
-const service=new Service()
-export default service;
+
+const service = new Service()
+export default service
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import EditPost from "./pages/EditPost";
 
 import Post from "./pages/Post";
 
-import AllPosts from "./pages/AllPost";
+import AllPosts from "./pages/AllPosts";
 
 const router = createBrowserRouter([
   {
